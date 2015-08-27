83a86e0629fed7d6ff185fbd14146fd307c6af62
alarin <me@alarin.ru>
2015-08-27 16:01:25 +0300

    [-] removed unused localy data

--- _ios_ci/scripts/options.sh
+++ _ios_ci/scripts/options.sh
@@ -1,19 +1,12 @@
+#options for local test run
+
 SCRIPTS=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 TESTS=$SCRIPTS/../tests
-
 TMPDIR=$SCRIPTS/../tmp
-
 CIRCLE_ARTIFACTS=$TMPDIR/artifacts
 BUILD_DIR=$TMPDIR/build
 
-DEVELOPER_NAME='iPhone Developer: Wayd Timashev'
 APPNAME='ios-ci-test'
 
 XCODE_WORKSPACE='ios-ci-test.xcworkspace'
 XCODE_SCHEME=ios-ci-test
-
-KEY_PASSWORD='Qwsa12!@'
-KEYCHAIN_PASSWORD='12345'
-
-CRASHLYTICS_API=4099e04cfd22fa4513e9445f4da8961dd648a4af
-CRASHLYTICS_SECRET=e8b4390c83b09feef763636086946e3174be194ccefbfccd89390b19d67dfc0f
